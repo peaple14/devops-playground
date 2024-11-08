61d9bfdea1d591831ba94680b72971e68038ddb6
peaple14 <whwo1214@gmail.com>
2024-11-08 11:19:03 +0900

    first push

---
+++ src/main/java/com/example/devopsplayground/DevopsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopsplayground/DevopsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
