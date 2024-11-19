b7f73eec9e0b8b52bf0426dec9729925f59ba169
peaple14 <whwo1214@gmail.com>
2024-11-19 09:59:29 +0900

    기본 ci/cd

--- src/main/java/com/example/devopsplayground/TestController.java
+++ src/main/java/com/example/devopsplayground/TestController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
-    public String test(){
+    public String test() {
         return "hello";
+    }
+
+    @GetMapping("/test")
+    public String test2(){
+        return "돌아가는중입니다.";
     }
 }
